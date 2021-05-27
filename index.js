d7f6041bcf964c6bf792494d01e151ed4bbb9789
Rushikesh Badgujar <rushikesh.badgujar9975@gmail.com>
2021-05-27 13:36:13 +0530

    enforced https and added resolving / links attempt 2

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const { ApolloServer, PubSub } = require('apollo-server-express');
 const mongoose = require('mongoose');
 const path = require("path");
 const db = require('./utils/mongodb')
-var compression = require('compression')
+const compression = require('compression')
+const enforce = require('express-sslify');
 require('dotenv').config()
 
 
@@ -15,6 +16,7 @@ const pubsub = new PubSub();
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
