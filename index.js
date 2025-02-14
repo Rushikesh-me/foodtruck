4449667e7281034d39a1a70661563fe02cdc330c
Rushikesh Badgujar <rushikesh.badgujar9975@gmail.com>
2025-02-14 13:54:34 +0000

    CORS update

--- index.js
+++ index.js
@@ -39,12 +39,6 @@ const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ 
         }
     })
   });
-
-   app.use(express.static(path.join(__dirname, "./client/build")));
-
-   app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-   });
   
 // app.listen(port, () => { console.log(`Server running at http://localhost:${port}${server.graphqlPath}`) });
 
